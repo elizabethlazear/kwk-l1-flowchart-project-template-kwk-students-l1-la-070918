59de3893b686d5b5107da79a4bf3b8f11f0c5305
elizabethlazear <elizabethlazear04@gmail.com>
2018-07-20 18:35:31 +0000

    automatically backed up by learn

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,13 @@
 class Quiz
 
+2.) Did you have a good day? yes, sort of, no
+3.) Did you feel supported by friends, family, others? yes, sort of, no
+*did you feel overwhelmed? yes, sort of, no
+*was it hard for you to consintrate? yes, sort of, no
+*was it hard to make a discision? yes, sort of, no
+1.) How did you feel today? happy, neutral, sad
+
+
+
+
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,13 @@
 class Result
 
+you should make one of these if you are ever having a bade day: 
+
+https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjAzLGSoazcAhVP5awKHQNhD7kQjRx6BAgBEAU&url=https%3A%2F%2Fmedium.com%2Fcall-me-a-theorist%2Femergency-care-wall-self-care-tips-that-work-321916ba3ac3&psig=AOvVaw367SW58bq5TRz-ct2DQxFH&ust=1532127308406946
+
+remeber:
+https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjKgt2HoqzcAhUMZKwKHeD_C9sQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F528961918715240895%2F&psig=AOvVaw1WEmtSFVt2DHzOKx6B0A9B&ust=1532127546897339
+
+if you are feeling sad: take a walk/get fresh air, turn up your favorite song and sing really loud even if you cannott sing, be around people you love, read, watch your favorite movie, take a bubble bath/long shower, 
+
+
 end
